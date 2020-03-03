70c391b66a7c6dfd102830293be238d2e537aa02
Virginie Montalibet <vmontalibet@emi.u-bordeaux1.fr>
2020-03-03 02:13:58 +0100

    remove a lot of loop to improve speed process

--- FDSystem.m
+++ FDSystem.m
@@ -13,7 +13,7 @@ classdef FDSystem <handle
         function obj =  FDSystem(mask)
             [x,y]= size(mask.matrix);
             obj.size_matrix= x*y;
-            obj.matrix = zeros(obj.size_matrix, obj.size_matrix);
+            
             obj.vector = zeros(obj.size_matrix, 1);
             obj.i_vect = [];
             obj.j_vect = [];
@@ -29,73 +29,88 @@ classdef FDSystem <handle
             self.matrix = toeplitz([x(1) fliplr(x(2:end))], x);
         end
         
-        function [i_four, j_four] = add_four(self, i,j, mask) 
-            i_four = (j-1)*size(mask.matrix,1)+i;
-            j_four = i_four;            
-            self.i_vect = [self.i_vect, i_four];
-            self.j_vect = [self.j_vect,j_four];
-            self.v_vect = [self.v_vect, -4];
+        function add_four(self, i,j, mask)
+%
+              self.i_vect = [self.i_vect, i'];
+              self.j_vect = [self.j_vect, j'];
+              n_vec = ones(1,size(i,1));
+              self.v_vect = [self.v_vect, -4.*n_vec];
            
         end
         
         function pix_up_is_inside(self, i_four, j_four)
-            self.i_vect = [ self.i_vect, i_four];
-            self.j_vect = [ self.j_vect, j_four-1];
-            self.v_vect = [ self.v_vect, 1];
+            n_vec = ones(1, size(i_four,1));
+            j_four(:) = j_four(:)-1;
+            self.i_vect = [ self.i_vect, i_four'];          
+            self.j_vect = [ self.j_vect, j_four'];
+             self.v_vect = [ self.v_vect, n_vec];
              
         end
         
         function pix_down_is_inside(self, i_four, j_four)
-            self.i_vect = [ self.i_vect, i_four];
-            self.j_vect = [ self.j_vect, j_four+1];
-            self.v_vect = [ self.v_vect, 1];
+
+            j_four(:) = j_four(:)+1;
+            n_vec = ones(1, size(i_four,1));
+            self.i_vect = [ self.i_vect, i_four'];
+            self.j_vect = [ self.j_vect, j_four'];
+            self.v_vect = [ self.v_vect, n_vec];
         end
         
         function pix_right_is_inside(self, i_four, j_four, mask)
-           
-            self.i_vect = [ self.i_vect, i_four];
-            self.j_vect = [ self.j_vect, j_four+ size(mask.matrix,1)];
-            self.v_vect = [ self.v_vect, 1];
+
+            j_four(:) = j_four(:)+ size(mask.matrix,1);
+            n_vec = ones(1, size(i_four,1));
+            self.i_vect = [ self.i_vect, i_four'];
+            self.j_vect = [ self.j_vect, j_four'];
+            self.v_vect = [ self.v_vect, n_vec];
         end
         
         function pix_left_is_inside(self, i_four, j_four, mask)
             
-            self.i_vect = [ self.i_vect, i_four];
-            self.j_vect = [ self.j_vect, j_four- size(mask.matrix,1)];
-            self.v_vect = [ self.v_vect, 1];
+            j_four(:)= j_four(:)-size(mask.matrix,1);
+            n_vec = ones(1, size(i_four,1));
+            self.i_vect = [ self.i_vect, i_four'];
+            self.j_vect = [ self.j_vect, j_four'];
+            self.v_vect = [ self.v_vect, n_vec];
+            
+            
         end
         
         function is_inside(self, i, j, mask)
-            [i_four, j_four] = self.add_four(i,j, mask);
-            self.pix_right_is_inside(i_four,j_four,mask);   
-             self.pix_left_is_inside(i_four,j_four,mask); 
-              self.pix_up_is_inside(i_four,j_four); 
-               self.pix_down_is_inside(i_four,j_four); 
+             self.add_four(i,j, mask);
+            self.pix_right_is_inside(i,j,mask);   
+             self.pix_left_is_inside(i,j,mask); 
+              self.pix_up_is_inside(i,j); 
+               self.pix_down_is_inside(i,j); 
                
         end
         
         function find_roi(self, mask)
-           [row, col]= find(mask.matrix==1);
-           for k = 1:size(row)
-            self.is_inside(row(k), col(k), mask);
-           end
+           [row, col]= find(mask.matrix);
+           row(:)= (col(:)-1).*size(mask.matrix,1)+row(:);
+           col(:) = row(:);
+           
+           self.is_inside(row(:), col(:), mask);
+          
         end
+       
         
-        function find_bound(self, mask)
-            [row, col] = find(mask.matrix==0.5);
-            for k = 1:size(row)
-                self.is_inside(row(k), col(k), mask);
-            end
-        end
         function find_useless(self, mask)
             [row,col] = find(mask.matrix ==0);
             for i = 1:size(row)
                 k = (col(i)-1)*size(mask.matrix,1)+row(i);
-                self.i_vect = [self.i_vect, k];
-                self.j_vect = [self.j_vect, k];
-                self.v_vect = [self.v_vect, 1];
                 self.vector(k) = mask.cut_im(row(i), col(i));
             end
+                row(:)= (col(:)-1).*size(mask.matrix,1)+row(:);
+                col(:) = row(:);
+                n_vec =ones(1, size(row,1));
+                self.i_vect = [self.i_vect, row'];
+                self.j_vect = [self.j_vect, col'];
+                self.v_vect = [self.v_vect, n_vec];
+               
+            
+            
+            
         end
                 
         
@@ -103,7 +118,7 @@ classdef FDSystem <handle
         function work_on_boundaries(self, mask)
             
             pixminusi = mask.boundaries(:,1)-1;
-            save('matrix.mat', 'pixminusi');
+   
             pixminusj = mask.boundaries(:,2)-1;
             pixplusi = mask.boundaries(:,1)+1;
             pixplusj = mask.boundaries(:,2)+1;
@@ -149,7 +164,7 @@ classdef FDSystem <handle
         function create_matrix(self, mask, image)
             self.compute_laplacian(image);
             self.find_roi(mask);
-            self.find_bound(mask);
+            %self.find_bound(mask);
             self.find_useless(mask);
             %self.work_on_boundaries(mask);
             self.matrix = sparse(self.i_vect, self.j_vect, self.v_vect, self.size_matrix, self.size_matrix);
